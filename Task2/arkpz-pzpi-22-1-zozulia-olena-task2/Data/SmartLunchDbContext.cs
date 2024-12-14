ef8786b02a5150692369eea125a7e04715256188
Olena Zozulia <olena.zozulia@nure.ua>
2024-12-14 20:13:19 +0200

    Add DbContext

---
+++ Task2/arkpz-pzpi-22-1-zozulia-olena-task2/Data/SmartLunchDbContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data
+{
+    public class SmartLunchDbContext : DbContext
+    {
+        public SmartLunchDbContext(DbContextOptions<SmartLunchDbContext> options) 
+            : base(options) 
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<FoodItem> FoodItems { get; set; }
+        public DbSet<Fridge> Fridges { get; set; }
+        public DbSet<FridgeInventory> FridgeInventories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>()
+                .HasMany(c => c.Employees)
+                .WithOne()
+                .HasForeignKey(u => u.CompanyId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<FridgeInventory>()
+                .HasOne(fi => fi.FoodItem)
+                .WithMany()
+                .HasForeignKey(fi => fi.FoodItemId);
+
+            modelBuilder.Entity<FridgeInventory>()
+                .HasOne<Fridge>()
+                .WithMany()
+                .HasForeignKey(fi => fi.FridgeId);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.FridgeInventory)
+                .WithMany()
+                .HasForeignKey(oi => oi.FridgeInventoryId);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.Items)
+                .WithOne()
+                .HasForeignKey(oi => oi.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Role)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.PaymentStatus)
+                .HasConversion<string>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
