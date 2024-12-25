997d1fdb9dabd16b0a3a949664dc83607c788d58
Olena Zozulia <olena.zozulia@nure.ua>
2024-12-25 21:24:53 +0200

    Add helpers

---
+++ Task3/arkpz-pzpi-22-1-zozulia-olena-task3/Helpers/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Models;
+using Models.DTO;
+
+namespace Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Company, CompanyDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<Notification, NotificationDto>().ReverseMap();
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Fridge, FridgeDto>().ReverseMap();
+            CreateMap<FoodItem, FoodItemDto>().ReverseMap();
+            CreateMap<FridgeInventory, FridgeInventoryDto>().ReverseMap();
+        }
+    }
+}
